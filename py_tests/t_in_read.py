939dee435d5cccc0f6efa40556c4e94020b856da
Rimas Baltrušis <30409733+rimelis@users.noreply.github.com>
2017-07-28 15:09:29 +0300

    test

--- py_tests/t_in_read.py
+++ py_tests/t_in_read.py
@@ -7,13 +7,14 @@ import time
 exitFlag = 0
 
 class ProcessInputThread (threading.Thread):
-    def __init__(self, p_irq_name, p_gpio_name, p_func_name):
+    def __init__(self, p_gpio_name, p_func_name):
         threading.Thread.__init__(self)
-        self.__title= p_irq_name + "(" + p_gpio_name + ")"
-        self.__irq= "cat /proc/interrupts | grep " + p_irq_name
-        self.__prev_irq_count= None
-        self.__curr_irq_count= None
-        self.__gpio= "cat /sys/class/gpio/" + p_gpio_name + "/value" 
+#        self.__title= p_irq_name + "(" + p_gpio_name + ")"
+#        self.__irq= "cat /proc/interrupts | grep " + p_irq_name
+#        self.__prev_irq_count= None
+#        self.__curr_irq_count= None
+#        self.__gpio_file= "/sys/class/gpio/" + p_gpio_name + "/value"
+        self.__file= open("/sys/class/gpio/" + p_gpio_name + "/value", "r+")
         self.__prev_value= None
         self.__curr_value= None
         self.__debounce_counter= 0
@@ -21,6 +22,27 @@ class ProcessInputThread (threading.Thread):
     def run(self):
         print ("Starting " + self.__title)
         while not exitFlag:
+            self.__file.seek(0, 0)
+            self.__curr_value= int(self.__file.read(1))
+            if self.__curr_value != self.__prev_value :
+                self.__debounce_counter= 2
+                while self.__debounce_counter > 0 :
+                    self.__file.seek(0, 0)
+                    self.__curr_value= int(self.__file.read(1))
+                    if self.__prev_value == self.__curr_value :
+                        self.__debounce_counter -= 1
+                    else :
+                        self.__prev_value= self.__curr_value
+                        self.__debounce_counter= 2
+                    time.sleep(0.1)
+
+            # Reiksme pasikeite
+            print ("%s: %s %s" % (self.__title, time.ctime(time.time()), self.__curr_value))
+            globals()[self.__func_name](self.__curr_value)
+            time.sleep(0.1)
+        print ("Exiting " + self.__title)
+
+          '''
           cmd_out= subprocess.check_output(self.__irq, shell=True)
           cmd_out_array= cmd_out.split()
           self.__curr_irq_count= int(cmd_out_array[1])
@@ -32,31 +54,27 @@ class ProcessInputThread (threading.Thread):
               self.__curr_value= int(cmd_out)
               if self.__prev_value == self.__curr_value :
                 self.__debounce_counter -= 1
-              else :    
+              else :
                 self.__prev_value= self.__curr_value
                 self.__debounce_counter= 2
               time.sleep(0.1)
-            # Reiksme pasikeite    
-            print ("%s: %s %s" % (self.__title, time.ctime(time.time()), self.__curr_value))
-            globals()[self.__func_name](self.__curr_value)
-          time.sleep(0.1)
-        print ("Exiting " + self.__title)
+            '''
 
 def SwitchOutput(p_gpio_name, p_value) :
   if p_value == 0 :
     l_value= 1
   else :
-    l_value= 0           
+    l_value= 0
   l_call= 'echo ' + str(l_value) + ' > /sys/class/gpio/' + p_gpio_name + '/value'
-  cmd_out= subprocess.check_call(l_call, shell=True)   
+  cmd_out= subprocess.check_call(l_call, shell=True)
 
 def TestSwitch(p_value) :
-  SwitchOutput("gpio26", p_value)
-    
+  dummy= 'foo'
+
 
 threads = []
 
-thread = ProcessInputThread("alarm_in_4", "gpio70", "TestSwitch")
+thread = ProcessInputThread("gpio69", "TestSwitch")
 threads.append(thread)
 
 # Start new Threads
@@ -79,6 +97,3 @@ irq_count= int(cmd_out_array[1])
 
 print(irq_count)
 '''
-        
-        
-        
