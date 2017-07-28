d381cd248c21a631ec6c351452ba05dcab663f38
Rimas Baltrušis <30409733+rimelis@users.noreply.github.com>
2017-07-28 11:25:22 +0300

    file

---
+++ py_tests/file1.py
@@ -0,0 +1,11 @@
+import mmap
+
+with open("/sys/class/gpio/gpio69/value", "r") as f:
+    map = mmap.mmap(f.fileno(), 0)
+    print(map.read_byte())
+    map.flush()
+    map.seek(0)
+    print(map.read_byte())
+    map.close()
+
+    
