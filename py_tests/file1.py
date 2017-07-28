95c606b000c32e94b7437064b7b0f13c8ecbdb72
Rimas Baltrušis <30409733+rimelis@users.noreply.github.com>
2017-07-28 13:04:38 +0300

    test

--- py_tests/file1.py
+++ py_tests/file1.py
@@ -1,6 +1,6 @@
-import mmap, os
+import mmap
 
-with os.open("/sys/class/gpio/gpio69/value", os.O_RDONLY) as f:
+with open("/sys/class/gpio/gpio69/value", 'r') as f:
     map = mmap.mmap(f.fileno(), 0, mmap.MAP_PRIVATE, prot=mmap.PROT_READ)
     print(map.read_byte())
     map.flush()
