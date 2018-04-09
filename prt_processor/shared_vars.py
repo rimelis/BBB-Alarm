1dd21a74f66ee2ae94fdfa6ff330a287a21d5fce
rimas <rimelis@gmail.com>
2018-04-09 14:10:22 +0300

    Prt processor logging

--- prt_processor/shared_vars.py
+++ prt_processor/shared_vars.py
@@ -1,5 +1,3 @@
-from classes import Area, Zone, KeySwitch
-
-AreaList = [Area(x) for x in range(5)]
-ZoneList = [Zone(x) for x in range(48)]
-KeySwitchList = [KeySwitch(x) for x in range(8)]
+AreaList = []
+ZoneList = []
+KeySwitchList = []
